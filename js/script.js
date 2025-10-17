7ec73a58eb4df412496341b16b77e85a709fa356
gabrieeloliveira <gabrielholiveiraa@gmail.com>
2025-10-17 18:09:14 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function filtarLista () {
+    // pega o valor digitado
+    const inputSearch = document.getElementById('pesquisaInput')
+    const filtro = inputSearch.value.toLowerCase();
+
+    // pega todos os itens da lista
+    const itens = document.querySelectorAll('li')
+
+    // percorre todos os <li> e mostra ou esconde conforme o filtro
+    itens.forEach(li => {
+        const texto = li.textContent.toLowerCase();
+        li.style.display = texto.includes(filtro) ? '' : 'none';
+    } )
+
+    const posicaoOriginal = inputSearch.offsetTop; // salva a posição original
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > posicaoOriginal){
+        inputSearch.classList.add('fixo')
+    } else {
+        inputSearch.classList.remove('fixo')
+    }
+})
+}
+
